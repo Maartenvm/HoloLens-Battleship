d6bacfb2653ca8ea9039778194d1e5bcda0f2d71
maartenvm <maartenvm@gmail.com>
2016-09-13 15:38:52 +0200

    Project setup, spaceship models, gaze collider.

---
+++ Assets/Scripts/GazeHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeHandler : MonoBehaviour {
+    private GameObject _hitObject;
+
+    [SerializeField] private Material _hitMaterial;
+    private Material _oldMaterial;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray cameraRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(cameraRay, out hitInfo))
+        {
+            if (_hitObject == hitInfo.transform.gameObject)
+            {
+                return;
+            }
+            _hitObject = hitInfo.transform.gameObject;
+            var myRenderer = _hitObject.GetComponent<MeshRenderer>();
+            _oldMaterial = myRenderer.material;
+            myRenderer.material = _hitMaterial;
+        }
+        else
+        {
+            if (_hitObject == null)
+            {
+                return;
+            }
+            var myRenderer = _hitObject.GetComponent<MeshRenderer>();
+            myRenderer.material = _oldMaterial;
+        }
+    }
+}
