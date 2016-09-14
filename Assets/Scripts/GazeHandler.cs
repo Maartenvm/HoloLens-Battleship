6dcb6675846c8a04f9967a006937ca083a735826
maartenvm <maartenvm@gmail.com>
2016-09-14 15:52:09 +0200

    dragging spaceships 0.1

--- Assets/Scripts/GazeHandler.cs
+++ Assets/Scripts/GazeHandler.cs
@@ -1,19 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.VR.WSA.Input;
+using UnityEngine.Windows.Speech;
 
-public class GazeHandler : MonoBehaviour {
-    private GameObject _hitObject;
+#if !UNITY_WSA
+using Windows.UI.Input.Spatial;
+#endif
 
-    [SerializeField] private Material _hitMaterial;
+using System;
+
+public class GazeHandler : MonoBehaviour
+{
+    private GameObject _hitObject = null;
+    private GameObject _draggedObject = null;
+
+    [SerializeField]
+    private Material _hitMaterial;
     private Material _oldMaterial;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private GestureRecognizer _recognizer;
+    private KeywordRecognizer _speechRecognizer;
+    private GestureRecognizer _manipulationRecognizer;
+
+    private Vector3 _navigationRailsStartOffset = new Vector3(0, 0, 0);
+    private bool _holdState = false;
+    private GestureSettings _oldRecognizedGestures = (GestureSettings.Tap | GestureSettings.Hold);
+
+    private float _positionSnapMultiplier = 0.1f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        String[] recognizedWords = new String[] { "A", "B", "C", "1", "2", "3", "alpha", "beta", "gamma", "shoot" };
+        _speechRecognizer = new KeywordRecognizer(recognizedWords);
+        _speechRecognizer.OnPhraseRecognized += SpeechRecognizerOnSpeechRecognizedEvent;
+        _speechRecognizer.Start();
+
+        _recognizer = new GestureRecognizer();
+        _recognizer.SetRecognizableGestures(GestureSettings.Tap | GestureSettings.Hold | GestureSettings.ManipulationTranslate);
+
+        _recognizer.TappedEvent += RecognizerOnTappedEvent;
+        _recognizer.HoldStartedEvent += RecognizerOnHoldStartedEvent;
+        _recognizer.HoldCompletedEvent += RecognizerOnHoldCompletedEvent;
+
+        _recognizer.ManipulationStartedEvent += RecognizerOnManipulationStartedEvent;
+        _recognizer.ManipulationUpdatedEvent += RecognizerOnManipulationUpdatedEvent;
+        _recognizer.ManipulationCompletedEvent += RecognizerOnManipulationCompletedEvent;
+        _recognizer.ManipulationCanceledEvent += RecognizerOnManipulationCanceledEvent;
+
+        _recognizer.StartCapturingGestures();
+
+#if !UNITY_WSA
+        SpatialInteractionManager mgr = SpatialInteractionManager.GetForCurrentView();
+        mgr.InteractionDetected += ManagerOnInteractionDetected;
+#endif
+    }
+
+    private void SpeechRecognizerOnSpeechRecognizedEvent(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log("Recognized phrase:" + args);
+    }
+
+
+
+#if !UNITY_WSA
+    private void ManagerOnInteractionDetected(SpatialInteractionManager sender, SpatialInteractionDetectedEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+#endif
+
+    // Update is called once per frame
+    void Update()
+    {
         Ray cameraRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         RaycastHit hitInfo;
 
@@ -25,8 +86,12 @@ public class GazeHandler : MonoBehaviour {
             }
             _hitObject = hitInfo.transform.gameObject;
             var myRenderer = _hitObject.GetComponent<MeshRenderer>();
-            _oldMaterial = myRenderer.material;
-            myRenderer.material = _hitMaterial;
+
+            if (myRenderer != null)
+            {
+                _oldMaterial = myRenderer.material;
+                myRenderer.material = _hitMaterial;
+            }
         }
         else
         {
@@ -35,7 +100,88 @@ public class GazeHandler : MonoBehaviour {
                 return;
             }
             var myRenderer = _hitObject.GetComponent<MeshRenderer>();
-            myRenderer.material = _oldMaterial;
+            if (myRenderer != null)
+            {
+                myRenderer.material = _oldMaterial;
+            }
+
+            _hitObject = null;
         }
     }
+
+    private void RecognizerOnTappedEvent(InteractionSourceKind source, int tapCount, Ray straaltje)
+    {
+        //if (_hitObject != null)
+        //{
+        //    _hitObject.SendMessage("onHit");
+        //}
+
+    }
+
+    private void RecognizerOnHoldStartedEvent(InteractionSourceKind source, Ray headRay)
+    {
+        Debug.Log("Start Hold mode.");
+        _holdState = true;
+
+
+    }
+
+    private void RecognizerOnHoldCompletedEvent(InteractionSourceKind source, Ray headRay)
+    {
+        Debug.Log("End Hold mode.");
+
+        //_recognizer.StopCapturingGestures();
+
+        //_recognizer.SetRecognizableGestures(_oldRecognizedGestures);
+        //_recognizer.NavigationStartedEvent -= RecognizerOnNavigationStartedEvent;
+        //_recognizer.NavigationUpdatedEvent -= RecognizerOnNavigationUpdatedEvent;
+
+        //_recognizer.StartCapturingGestures();
+
+        _holdState = false;
+    }
+
+    private float roundToNearest(float input) {
+        return (float)Math.Round(Convert.ToDouble(input * 10f *_positionSnapMultiplier));
+    }
+
+    private void RecognizerOnManipulationUpdatedEvent(InteractionSourceKind source, Vector3 normalizedOffset, Ray headRay)
+    {
+        if (_holdState && _draggedObject != null)
+        {
+            Vector3 proposedPosition = _draggedObject.transform.position + (normalizedOffset - _navigationRailsStartOffset);
+            Vector3 newPosition = new Vector3(roundToNearest(proposedPosition.x), roundToNearest(proposedPosition.y), roundToNearest(proposedPosition.z));
+           
+            _draggedObject.transform.position = newPosition;
+        }
+        else
+        {
+            _holdState = true;
+            _navigationRailsStartOffset = normalizedOffset;
+        }
+    }
+
+    private void RecognizerOnManipulationStartedEvent(InteractionSourceKind source, Vector3 normalizedOffset, Ray headRay)
+    {
+        _holdState = true;
+        _draggedObject = _hitObject;
+        _navigationRailsStartOffset = normalizedOffset;
+    }
+
+    private void RecognizerOnManipulationCanceledEvent(InteractionSourceKind source, Vector3 normalizedOffset, Ray headRay)
+    {
+        _draggedObject = null;
+        _holdState = false;
+    }
+
+    private void RecognizerOnManipulationCompletedEvent(InteractionSourceKind source, Vector3 normalizedOffset, Ray headRay)
+    {
+        _draggedObject = null;
+        _holdState = false;
+    }
+
+
+
+
+
 }
---
+++ Assets/Scripts/Hittable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hittable : MonoBehaviour {
+    void onHit() {
+        Rigidbody rigid = GetComponent<Rigidbody>();
+        rigid.useGravity = true;
+    }
+
+    void onDrag() {
+
+    }
+}
